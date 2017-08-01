1eb9c48293eab8b923f4cbc7ced88c56fcf28101
chenpi <chenpi@2dfire>
2017-08-01 16:44:12 +0800

    反射练习

--- filePractice/src/main/MainClass.java
+++ filePractice/src/main/MainClass.java
@@ -2,43 +2,15 @@ package main;
 
 import other.FileUtil;
 
-import java.io.File;
-import java.util.List;
 
 /**
  * Created by Chenpi on 2017/7/13.
  */
 public class MainClass {
 
-    /***
-     * 读取一个文件夹下所有文件，并内容拼接到一个文件里
-     * https://zhidao.baidu.com/question/1368120362072340179.html
-     * @param args
-     */
     public static void main(String[] args) {
-
-        File dir = new File("E:/allText");
-
-        //查找文件
-        List<File> fileList = FileUtil.findFiles(dir, false);
-
-        File targetFile = new File("E:/allText/resultFile/人民的名义(全54章).txt");
-        if (FileUtil.createFile(targetFile.getPath())) {
-            System.out.println("成功生成目标文件：" + targetFile.getPath());
-        }
-
-        long startTime = System.currentTimeMillis();
-        for (File file : fileList) {
-            String content = FileUtil.readFile(file);
-
-            FileUtil.writeFile(targetFile,content);
-            System.out.println("处理完成："+file.getPath());
-        }
-        System.out.println("处理文件(个) " + fileList.size() + "，共用时 " + (System.currentTimeMillis() - startTime));
+    //    空白
     }
-
-
-
 
     /**
      * 用IO进行一个图片复制
